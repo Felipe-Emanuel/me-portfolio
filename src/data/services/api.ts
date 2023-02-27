147ad275d9a6735a95a4e172cf8c6d198eaa6236
Felipe <felipesullivan50@gmail.com>
2023-02-27 18:57:50 -0300

    api base url

--- src/data/services/api.ts
+++ src/data/services/api.ts
@@ -1,15 +1,7 @@
 import axios from 'axios';
 
-// const api = axios.create({
-//     baseURL: 'https://meportfolio-nine.vercel.app/'
-// })
-
-// export default api
-
-// DEV REQUEST
-
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://meportfolio-nine.vercel.app/'
 })
 
 export default api
